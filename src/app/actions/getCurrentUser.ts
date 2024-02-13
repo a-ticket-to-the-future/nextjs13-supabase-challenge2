1553aaca7ebe1703550334cea3b96c5c421a94ba
片倉秀登 <katakurashuto415@KatakuraShutoMacBook-Air.local>
2024-02-14 00:12:53 +0900

    seventh commit

---
+++ src/app/actions/getCurrentUser.ts
@@ -0,0 +1,35 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]/route";
+import prisma from '@/app/lib/prisma'
+
+
+//　ログインユーザー取得
+const getCurrentUser = async () => {
+
+    try {
+        // セッション情報取得
+        const session = await getServerSession(authOptions);
+
+        //　ログインしていない場合
+        if(!session?.user?.email){
+            return null
+        }
+
+        // ログインユーザー取得
+        const response = await prisma.user.findUnique({
+            where: {
+                email: session.user.email,
+            },
+        })
+
+        if (!response) {
+            return null
+        }
+
+    } catch (error) {
+        return null
+    }
+ 
+}
+
+export default getCurrentUser
