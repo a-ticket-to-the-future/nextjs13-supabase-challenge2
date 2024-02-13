df715614aabba72d68184512e80717ebeffd82b0
片倉秀登 <katakurashuto415@KatakuraShutoMacBook-Air.local>
2024-02-13 15:40:29 +0900

    third commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <div>
-      <div>Main</div>
+      <div className=' ml-[80px] text-center'>Main</div>
     </div>
   )
 }
